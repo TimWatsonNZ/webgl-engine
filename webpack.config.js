6e0c189397e9ff17b282c0c14fdf08fa2d83472c
Tim Watson <tim.watson.nz@gmail.com>
2020-04-02 14:02:04 +1300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  mode: 'development',
+  resolve: {
+    extensions: [ '.ts', '.js']
+  }
+};
